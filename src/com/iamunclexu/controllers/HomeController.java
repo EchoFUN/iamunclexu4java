0e2d02d30f24c7cd0e26664844e3df70fc569dcf
EchoFUN <xukai.ken@gmail.com>
2019-05-15 20:43:20 +0800

    ADD

--- src/com/iamunclexu/controllers/HomeController.java
+++ src/com/iamunclexu/controllers/HomeController.java
@@ -1,13 +1,57 @@
 package com.iamunclexu.controllers;
 
+import com.iamunclexu.database.DBUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 
 public class HomeController extends Controller {
+    private static Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @Override
     public HttpResponse process(HttpRequest request) {
+        String output = "";
 
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBUtils.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select id from post");
 
-        return null;
+            while (resultSet.next()) {
+                output += String.valueOf(resultSet.getInt("id"));
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    DBUtils.releaseConnection(connection);
+                }
+            } catch (SQLException e) {
+                LOGGER.error(e.getMessage());
+            }
+        }
+        return new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(output.getBytes()));
     }
 }
