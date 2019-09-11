1e4b3994ff05d06714f8afeec80d036f3f90607e
EchoFUN <xukai.ken@gmail.com>
2019-09-11 12:44:52 +0800

    ADD

--- src/com/iamunclexu/controllers/GeneralInfoController.kt
+++ src/com/iamunclexu/controllers/GeneralInfoController.kt
@@ -1,6 +1,7 @@
 package com.iamunclexu.controllers
 
 import com.google.gson.JsonObject
+import com.iamunclexu.database.CommentModel
 import io.netty.buffer.Unpooled
 import io.netty.handler.codec.http.*
 import org.slf4j.LoggerFactory
@@ -9,14 +10,16 @@ import io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE
 
 class GeneralInfoController : Controller() {
     private var logger = LoggerFactory.getLogger(GeneralInfoController::class.java)
+    private var commentModel = CommentModel()
 
     override fun process(request: HttpRequest?): HttpResponse {
         logger.info("hi")
         var json = JsonObject()
-        json.addProperty("2", 2)
+        json.addProperty("hello", "world!")
 
+        var unapproved = commentModel.fetchUnApproved()
 
-        var response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(json.toString().toByteArray()))
+        var response = DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer(unapproved.toString().toByteArray()))
         val headers = response.headers()
         headers.add(CONTENT_TYPE, "application/json; charset=utf-8")
 
